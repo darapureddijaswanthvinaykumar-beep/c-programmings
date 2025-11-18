cb841cdec96cb359ffc30bf79f4f8d5d740f8950
darapureddijaswanthvinaykumar-beep <darapureddijaswanthvinaykumar@gmail.com>
2025-11-18 14:22:50 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void mul(int,int);
+int main(){
+    int x=2,y=2;
+        mul(x,y);
+
+    return 0;
+    
+}
+void mul(int x,int y)
+{
+    int z;
+    z = x*y;
+    printf("mul=%d",z);
+    
+}
